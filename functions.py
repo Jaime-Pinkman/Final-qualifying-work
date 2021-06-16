93b2fa592d2ad6faae4ddc5b6bf6315aa1fb0aee
Isangulov Marat <37271333+Jaime-Pinkman@users.noreply.github.com>
2021-06-16 23:48:25 +0300

    refactored file

--- functions.py
+++ functions.py
@@ -20,18 +20,18 @@ rcParams['figure.figsize'] = 18, 8
 from sklearn.preprocessing import MinMaxScaler
 
 def split_servers(df_local):
-    df_local = df_local.copy()
-    servers = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
-    df_server = {}
-    for i in range(len(servers)):
-        df_server[servers[i]] = df_local.iloc[i::len(servers), :]
-        df_server[servers[i]].reset_index(drop=True, inplace=True)
-        df_server[servers[i]] = df_server[servers[i]].add_prefix(servers[i] + '_')
-        df_server[servers[i]].drop([servers[i] + '_'+'m_id'], axis=1, inplace=True)
-        df_server[servers[i]].rename(columns={servers[i] + '_'+'sample_time': 'sample_time'}, inplace=True)
-    df_concat = pd.concat(list(df_server.values()), axis=1)
-    df_concat = df_concat.loc[:,~df_concat.columns.duplicated()]
-    return df_concat
+	df_local = df_local.copy()
+	servers = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+	df_server = {}
+	for i in range(len(servers)):
+		df_server[servers[i]] = df_local.iloc[i::len(servers), :]
+		df_server[servers[i]].reset_index(drop=True, inplace=True)
+		df_server[servers[i]] = df_server[servers[i]].add_prefix(servers[i] + '_')
+		df_server[servers[i]].drop([servers[i] + '_'+'m_id'], axis=1, inplace=True)
+		df_server[servers[i]].rename(columns={servers[i] + '_'+'sample_time': 'sample_time'}, inplace=True)
+	df_concat = pd.concat(list(df_server.values()), axis=1)
+	df_concat = df_concat.loc[:,~df_concat.columns.duplicated()]
+	return df_concat
     
 def series_to_supervised(data, columns, n_in=1, n_out=1, dropnan=True):
 	n_vars = 1 if type(data) is list else data.shape[1]
@@ -57,42 +57,42 @@ def series_to_supervised(data, columns, n_in=1, n_out=1, dropnan=True):
 	return agg
   
 def get_dataset_concat(df_local, server, n_in=1, n_out=1):
-    df_local = df_local.copy()
-    #df_local.drop(['sample_time'], axis=1, inplace=True)
-    values = df_local.values
+	df_local = df_local.copy()
+	#df_local.drop(['sample_time'], axis=1, inplace=True)
+	values = df_local.values
 
-    data = series_to_supervised(values, df_local.keys(), n_in, n_out)
-    target_0 = data[server+'_'+'cpu_01_busy'+'(t)']
-    target_1 = data[server+'_'+'cpu_01_busy'+'(t+1)']
-    target_2 = data[server+'_'+'cpu_01_busy'+'(t+2)']
-    target_3 = data[server+'_'+'cpu_01_busy'+'(t+3)']
-    target_4 = data[server+'_'+'cpu_01_busy'+'(t+4)']
+	data = series_to_supervised(values, df_local.keys(), n_in, n_out)
+	target_0 = data[server+'_'+'cpu_01_busy'+'(t)']
+	target_1 = data[server+'_'+'cpu_01_busy'+'(t+1)']
+	target_2 = data[server+'_'+'cpu_01_busy'+'(t+2)']
+	target_3 = data[server+'_'+'cpu_01_busy'+'(t+3)']
+	target_4 = data[server+'_'+'cpu_01_busy'+'(t+4)']
 
-    target = pd.concat([target_0, target_1, target_2, target_3, target_4], axis = 1)#, target_3, target_4]
+	target = pd.concat([target_0, target_1, target_2, target_3, target_4], axis = 1)#, target_3, target_4]
 
-    data = data.loc[:,~data.columns.str.endswith('(t)')]
-    data = data.loc[:,~data.columns.str.endswith('(t+1)')]
-    data = data.loc[:,~data.columns.str.endswith('(t+2)')]
-    data = data.loc[:,~data.columns.str.endswith('(t+3)')]
-    data = data.loc[:,~data.columns.str.endswith('(t+4)')]
-    return data, target
-    
+	data = data.loc[:,~data.columns.str.endswith('(t)')]
+	data = data.loc[:,~data.columns.str.endswith('(t+1)')]
+	data = data.loc[:,~data.columns.str.endswith('(t+2)')]
+	data = data.loc[:,~data.columns.str.endswith('(t+3)')]
+	data = data.loc[:,~data.columns.str.endswith('(t+4)')]
+	return data, target
+
 def difference(dataset, interval=1):
-  diff = list()
-  dataset = dataset.values.copy()
-  for i in range(interval, len(dataset)):
-   value = dataset[i] - dataset[i - interval]
-   diff.append(value)
-  return diff
+	diff = list()
+	dataset = dataset.values.copy()
+	for i in range(interval, len(dataset)):
+		value = dataset[i] - dataset[i - interval]
+		diff.append(value)
+	return diff
 
 # invert differenced forecast
 def inverse_difference(last_ob, value):
 	return value + last_ob
   
 def eat_and_produce(model, data):
-  preds = model.predict(data)
-  print(np.shape(preds))
-  return preds
+	preds = model.predict(data)
+	print(np.shape(preds))
+	return preds
 
 def root_mean_squared_error(y_true, y_pred):
-  return K.sqrt(K.mean(K.square(y_pred - y_true)))
+	return K.sqrt(K.mean(K.square(y_pred - y_true)))
